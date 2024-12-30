e04c9eba83b71ed0c458d03bd98c166f8df8ed98
Fran Martinez <martinezcoder@gmail.com>
2024-12-30 17:11:55 +0100

    Prevent the creation of duplicated monitors

--- lib/datadog_exporter/monitors/import.rb
+++ lib/datadog_exporter/monitors/import.rb
@@ -10,13 +10,19 @@ module DatadogExporter
         client: DatadogExporter::Client.new(config:),
         request: DatadogExporter::DatadogApiRequests::Monitors.new(config:, client:),
         template_manager: Utilities::TemplateManager.new(config:),
-        file_class: File
+        file_class: File,
+        existent_monitors_service: DatadogExporter::Monitors::Export.new
       )
         @config = config
         @request = request
         @monitors_base_path = config.base_path.join(DatadogExporter::Monitors::EXPORT_FOLDER)
         @template_manager = template_manager
         @file_class = file_class
+        @existent_monitors_service = existent_monitors_service
+      end
+
+      def list(tag: nil, &)
+        monitors.select { |monitor| tag.nil? || monitor[:tags].include?(tag) }.each(&)
       end
 
       ##
@@ -25,6 +31,7 @@ module DatadogExporter
       #   * Loops all exported monitors in the base_path/monitors folder.
       #   * Transforms the monitor into a template with placeholders.
       #   * Replace the placeholders with the environment values
+      #   * Checks if the monitor already exists in the Datadog environment.
       #   * Creates the monitor in the Datadog environment.
       #
       # If a tag is provided, it imports only the monitors with that tag.
@@ -32,16 +39,37 @@ module DatadogExporter
       # @param [Symbol] to The environment (defined in your organizations_config_filename) where the monitors will be imported
       # @param [String] tag (optional) A tag defined in the Datadog monitors
       def import(to:, tag: nil)
-        monitors
-          .select { |monitor| tag.nil? || monitor[:tags].include?(tag) }
-          .each do |monitor|
-            template = @template_manager.create_template(monitor)
-            target_monitor = @template_manager.create_monitor(template, environment: to)
-            @request.create(target_monitor) if accepted?(target_monitor, to)
+        monitors = []
+
+        list(tag: tag) do |monitor|
+          template = @template_manager.create_template(monitor)
+          monitor = @template_manager.create_monitor(template, environment: to)
+
+          if exists?(monitor)
+            puts "Monitor `#{monitor[:name]}` already exists on #{to}"
+          elsif accepted?(monitor, to)
+            monitors << @request.create(monitor)
           end
+        end
+
+        puts "monitors created: #{monitors.count}"
       end
 
       private
+
+      def exists?(monitor_to_import)
+        existent_monitor =
+          existent_monitors.find { |monitor| monitor[:name] == monitor_to_import[:name] }
+
+        existent_monitor.inspect == monitor_to_import.inspect
+      end
+
+      def existent_monitors
+        @existent_monitors ||=
+          @existent_monitors_service.list.map do |monitor|
+            @template_manager.filter_by_template_keys(monitor)
+          end
+      end
 
       def monitors
         @monitors_base_path
